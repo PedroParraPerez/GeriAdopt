e135d6e6b9867c440e1731cd91664a815271cf7d
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-21 16:56:29 +0000

    update cards

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
+import { Card } from "../component/card";
 
 
 export const Home = () => {
@@ -10,6 +11,7 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
+     
 			<p>
 			</p>
      
