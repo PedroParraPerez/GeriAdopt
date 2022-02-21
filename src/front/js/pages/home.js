f061fb7f53b4353a09db5012e9301e9fb7a80e8a
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-21 12:42:19 +0000

    update

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,14 +2,12 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
-import { Card } from "../component/card";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-      <Card/>
 			<h1>Hello Rigo!!</h1>
 			<p>
 				
