3c7a4ad3c98f25bda8a143652b40c2de1ae9ad92
Sofia <sofiaxrk@gmail.com>
2022-02-24 12:05:20 +0000

    guardado

--- src/front/js/pages/PerfilProtectora.js
+++ src/front/js/pages/PerfilProtectora.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "../../styles/PerfilProtectora.css";
 
-import { Card } from "../component/card.js";
+import { CardProtectora } from "../component/cardProtectora.js";
 import SOSPeludos from "../../img/sospeludos.jpeg";
 import EditIcon from "../../img/editIcon.png";
 import ImgTitleFavs from "../../img/imagetitlefavs.png";
@@ -13,13 +13,21 @@ export const PerfilProtectora = () => {
       <div className="container-fluid">
         <div className="row Protectora_Protectoraprofile">
           <div className="col-xl-2 Protectora_imgprofile">
-            <img src={SOSPeludos} alt="profilephoto" className="Protectora_Protectoraphoto" />
+            <img
+              src={SOSPeludos}
+              alt="profilephoto"
+              className="Protectora_Protectoraphoto"
+            />
           </div>
           <div className="col-xl-4 Protectora_ProtectoraInfo">
             <div className="row Protectora_edit">
               <div className="col-xl-12">
                 <div className="Protectora_editbutton">
-                  <img src={EditIcon} className="Protectora_editicon" alt="editIcon" />
+                  <img
+                    src={EditIcon}
+                    className="Protectora_editicon"
+                    alt="editIcon"
+                  />
                 </div>
               </div>
             </div>
@@ -27,24 +35,33 @@ export const PerfilProtectora = () => {
               <div className="col-xl-6 Protectora_primayinfo">
                 <p>
                   <b>
-                    <span className="Protectora_labelprofileinfo">Nombre: </span>SOS Peludos España
+                    <span className="Protectora_labelprofileinfo">
+                      Nombre:{" "}
+                    </span>
+                    SOS Peludos España
                   </b>
                 </p>
                 <p>
                   <b>
-                    <span className="Protectora_labelprofileinfo">Ciudad: </span>Madrid
+                    <span className="Protectora_labelprofileinfo">
+                      Ciudad:{" "}
+                    </span>
+                    Madrid
                   </b>
                 </p>
               </div>
               <div className="col-xl-6 Protectora_secundaryinfo">
                 <p>
                   <b>
-                    <span className="Protectora_labelprofileinfo">Tel: </span>666554477
+                    <span className="Protectora_labelprofileinfo">Tel: </span>
+                    666554477
                   </b>
                 </p>
                 <p>
                   <b>
-                    <span className="Protectora_labelprofileinfo">Correo: </span>
+                    <span className="Protectora_labelprofileinfo">
+                      Correo:{" "}
+                    </span>
                     sospeludos.es@gmail.com
                   </b>
                 </p>
@@ -54,7 +71,11 @@ export const PerfilProtectora = () => {
         </div>
         <div className="row Protectora_titleFavs">
           <div className="col-xl-1">
-            <img src={ImgTitleFavs} alt="titlecat" className="Protectora_ImgTitleFavs" />
+            <img
+              src={ImgTitleFavs}
+              alt="titlecat"
+              className="Protectora_ImgTitleFavs"
+            />
           </div>
           <div className="col-xl-2">
             <h2 className="titleProte">
@@ -63,15 +84,17 @@ export const PerfilProtectora = () => {
           </div>
         </div>
         <div className="row Protectora_cardlist">
-          <CardProtectora /> <CardProtectora /> <CardProtectora /> <CardProtectora />
+          <CardProtectora /> <CardProtectora /> <CardProtectora />{" "}
+          <CardProtectora />
         </div>
         <div className="row Protectora_cardProtectoralist">
-          <CardProtectora /> <CardProtectora /> <CardProtectora /> <CardProtectora />
+          <CardProtectora /> <CardProtectora /> <CardProtectora />{" "}
+          <CardProtectora />
         </div>
         <div className="row Protectora_cardProtectoralist">
-          <CardProtectora /> <CardProtectora /> <CardProtectora /> <CardProtectora />
+          <CardProtectora /> <CardProtectora /> <CardProtectora />{" "}
+          <CardProtectora />
         </div>
-        
       </div>
     </>
   );
