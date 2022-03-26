91a9bcb60e0033ee7d1a1dd72e2ccb37c31da32f
PedroParraPerez <pedroparra06091994@gmail.com>
2022-03-26 01:45:17 +0000

    update css buttons edit profile image

--- src/front/js/component/buttonsForEditProfilePhoto.js
+++ src/front/js/component/buttonsForEditProfilePhoto.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import ProfileImgGeneric from "../../img/profile_image_generic.jpg";
+
 import "../../styles/buttonsforeditprofilephoto.css";
 export const ButtonsForEditProfilePhoto = () => {
+  const { store, actions } = useContext(Context);
+
   return (
     <>
       <div className="button_edit_photo_profile d-block">
@@ -8,7 +13,7 @@ export const ButtonsForEditProfilePhoto = () => {
           <div className="col-xl-12 d-block">
             <label class="file-upload">
               <input type="file" multiple="multiple" name="fileToUpload" />
-              Modificar
+              {store.shelterInfo.image ? "Modificar" : "Añadir imagen"}
             </label>
 
             <button type="button" className="buttoneditphoto">
